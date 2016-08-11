27f386d3b9163edd1dec4360cddb716418ed2de0
My Nguyen <My@mys-macbook-pro.local>
2016-08-11 15:10:43 -0700

    Designing the interface

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeFilter(sender: AnyObject) {
+    }
 
+    @IBAction func save(sender: AnyObject) {
+    }
+
+    @IBAction func intensityChanged(sender: AnyObject) {
+    }
 }
 
